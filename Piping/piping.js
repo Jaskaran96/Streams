ec7d87e4c7dc7389db3a355b5b07baf79a907bcb
Jaskaran Singh <bartychuck@gmail.com>
2024-04-01 02:08:08 +0530

    pipelining

--- Piping/piping.js
+++ Piping/piping.js
@@ -1,4 +1,5 @@
 const fs = require("node:fs/promises");
+const { pipeline } = require("node:stream");
 const SOURCE_FILE_PATH = "../source.txt";
 const DEST_FILE_PATH = "../destination.txt";
 
@@ -13,9 +14,19 @@ const DEST_FILE_PATH = "../destination.txt";
   //the destination of the pipe has to be a write stream
   //The readable.pipe() method attaches a Writable stream to the readable, causing it to switch automatically into flowing mode and push all of its data to the attached Writable. The flow of data will be automatically managed so that the destination Writable stream is not overwhelmed by a faster Readable stream.
 
-  readStream.pipe(writeSteam);
-  readStream.on("end", () => {
-    console.timeEnd("piping");
-    console.log("this was the end");
+  //The pipe methdo will not handle closing of the streams if there is an error while piping the data, thus we will use pipeline for that
+  // readStream.pipe(writeSteam);
+  // readStream.on("end", () => {
+  //   console.timeEnd("piping");
+  //   console.log("this was the end");
+  // });
+
+  pipeline(readStream, writeSteam, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.timeEnd("piping");
+      console.log("this was the end");
+    }
   });
 })();
